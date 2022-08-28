d9484be51f117d42d1c3251a2ad3f48f314bd99b
Yarden Sapir <yarden@Yardens-MacBook-Pro.local>
2022-08-28 18:48:50 +0300

    make it better

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import environments from "../config/environments.js";
 import connecToMongoDb from "./databases/mongoose.db.js";
 import userRouter from "./routers/user.router.js";
 import bookRouter from "./routers/book.router.js";
-import cartRouter from './routers/cart.router.js'
+import path from 'path'
+import cartRouter from "./routers/cart.router.js";
+
 
 dotenv.config();
 
@@ -15,13 +17,15 @@ app.use(express.json());
 app.use(cors());
 app.use(userRouter);
 app.use(bookRouter);
-app.use(cartRouter) 
+app.use(cartRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.get('/',(req,res)=>{
-  res.send('Hello from the server')
-})
-const PORT = environments.PORT;
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const PORT = environments.PORT || 3001;
 
 app.listen(PORT || 3001, async () => {
   console.log(`Server is running on PORT ${PORT}`);
