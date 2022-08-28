29fbf9996798b3bd15da42633b0858875de74c13
Yarden Sapir <yarden@Yardens-MacBook-Pro.local>
2022-08-28 18:54:41 +0300

    make it better

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import userRouter from "./routers/user.router.js";
 import bookRouter from "./routers/book.router.js";
 import cartRouter from "./routers/cart.router.js";
 
-const path = require('path')
 dotenv.config();
 
 const app = express();
@@ -17,13 +16,7 @@ app.use(cors());
 app.use(userRouter);
 app.use(bookRouter);
 app.use(cartRouter);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 const PORT = environments.PORT || 3001;
 
 app.listen(PORT || 3001, async () => {
